f98353c12f7c51ac7a23ca82dde0a15abb5fa5a8
John M. Wargo <johnwargo@users.noreply.github.com>
2022-11-12 16:45:08 -0500

    Update cats.ts

--- src/commands/cats.ts
+++ src/commands/cats.ts
@@ -1,6 +1,10 @@
 // List all of the categories in the export file
 
 import { Command, Flags } from '@oclif/core'
+import fs = require('fs')
+import path = require('path')
+
+const catFileRoot: string = "_categories.json"
 
 export default class Cats extends Command {
   static summary = 'Category List'
@@ -19,8 +23,8 @@ export default class Cats extends Command {
 
   static args = [
     {
-      name: 'folder',  required: true,
-      description: 'The name of the folder where the Joomla table export files are stored (use . for current folder).',
+      name: 'folder', required: true,
+      description: 'The name of the folder where the Joomla table export files are stored (use . for current folder).'
     },
     { name: 'prefix', required: true, description: 'Joomla table export file prefix.' }
   ]
@@ -34,9 +38,18 @@ export default class Cats extends Command {
       console.log('Debug mode enabled')
     }
 
-    const prefix: string = args.prefix
     const inputFolder: string = args.folder
     console.log(`Input folder: "${inputFolder}"`)
-    console.log(`File prefix: "${prefix}"`)
+    // does the input folder exist?
+    if (fs.existsSync(inputFolder)) {
+      console.log(`Input folder "${inputFolder}" exists.`)
+
+      const fileName = args.prefix + catFileRoot
+      const inputFile = inputFolder == '.' ? fileName : path.join(inputFolder, fileName)
+
+      console.log(`Input file: "${inputFile}"`)
+    } else {
+      console.log(`Input folder "${inputFolder}" does not exist.`)
+    }
   }
 }
