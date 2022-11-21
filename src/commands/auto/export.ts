f0374ae8045677275f66b0fb0f9aa38e715d10a3
John M. Wargo <johnwargo@users.noreply.github.com>
2022-11-21 17:31:43 -0500

    Working on `auto` options

--- src/commands/auto/export.ts
+++ src/commands/auto/export.ts
@@ -1,20 +1,13 @@
 import {Command, Flags} from '@oclif/core'
 
 export default class AutoExport extends Command {
-  static description = 'describe the command here'
+  static summary = 'Auto Export';
+  static description = 'Export Joomla article content to markdown files using the configuration options defined in the local configuration file.';
+  static aliases = ['ae'];
+  static examples = ['<%= config.bin %> <%= command.id %>'];
 
-  static examples = [
-    '<%= config.bin %> <%= command.id %>',
-  ]
-
-  static flags = {
-    // flag with a value (-n, --name=VALUE)
-    name: Flags.string({char: 'n', description: 'name to print'}),
-    // flag with no value (-f, --force)
-    force: Flags.boolean({char: 'f'}),
-  }
-
-  static args = [{name: 'file'}]
+  static flags = { debug: Flags.boolean({ char: 'd' }) };
+  static args = [];
 
   public async run(): Promise<void> {
     const {args, flags} = await this.parse(AutoExport)
--- src/commands/auto/init.ts
+++ src/commands/auto/init.ts
@@ -1,26 +1,21 @@
 import {Command, Flags} from '@oclif/core'
 
+// TODO: Prompt for all of the config values
+
 export default class AutoInit extends Command {
-  static description = 'describe the command here'
+  static summary = 'Auto Initialize';
+  static description = 'Create the local configuration file for the auto command.';
+  static aliases = ['ai'];
+  static examples = ['<%= config.bin %> <%= command.id %>'];
 
-  static examples = [
-    '<%= config.bin %> <%= command.id %>',
-  ]
-
-  static flags = {
-    // flag with a value (-n, --name=VALUE)
-    name: Flags.string({char: 'n', description: 'name to print'}),
-    // flag with no value (-f, --force)
-    force: Flags.boolean({char: 'f'}),
-  }
-
-  static args = [{name: 'file'}]
+  static flags = { debug: Flags.boolean({ char: 'd' }) };
+  static args = [];
 
   public async run(): Promise<void> {
-    const {args, flags} = await this.parse(AutoInit)
+    const {args, flags} = await this.parse(AutoInit);
 
-    const name = flags.name ?? 'world'
-    this.log(`hello ${name} from D:\\dev\\node\\joomla-to-markdown\\src\\commands\\auto\\init.ts`)
+    const name = flags.name ?? 'world';
+    this.log(`hello ${name} from D:\\dev\\node\\joomla-to-markdown\\src\\commands\\auto\\init.ts`);
     if (args.file && flags.force) {
       this.log(`you input --force and --file: ${args.file}`)
     }
